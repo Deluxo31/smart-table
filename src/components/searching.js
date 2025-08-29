3e4c8272ba66c5bcca16700754ae99e7bbdb8e13
Deluxo <deluxo31@gmail.com>
2025-08-29 23:29:29 +0300

    try 1

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,15 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison(
+        ['skipEmptyTargetValues'], // Массив имен правил
+        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)] // Пользовательские правила
+    );
+
+
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+         return data.filter(item => compare(item, state));
     }
 }
